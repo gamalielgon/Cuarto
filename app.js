5b20af2ab1e33ef944ec6171456a11ac57819f85
gamalielgon <ggonzalez26@ucol.mx>
2022-02-05 20:49:06 -0600

    Actividad terminada

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function logGreeting(fn) {
 };
 logGreeting(greet);
 
-//v1 funcion donde no necesitas declararla variable "Let"
+//v1 funcion donde no necesitas declarar la variable "Let"
 logGreeting(function(){console.log(`Hola buenas`)});
 //v2 funcion ES6
 logGreeting(()=>{console.log(`Si funciono`)});
@@ -30,11 +30,32 @@ let greetMe = function () {
 greetMe();
 logGreeting(greetMe);
 
-//probando funciones de ES6
+//probando funciones con parametros ES6 donde podemos sacar el cuadrado de cualquier numero
 let cuadrado = (x) => x*x;
 console.log(`${cuadrado(4)}`);
 cuadrado();
 
-//Funcion de saludo
+//Funcion de saludo donde se resive el nombre y el tratamiento
+//en las funciones de fat arrow se resiven parametros igual que en las normales
+//podemos no usar las llaves si solo tenemos una linea de codigo
 let saludo = (nombre, tratamiento) => console.log(`Buen día ${tratamiento}. ${nombre}`);
 saludo(`Juan`, `Sr`);
+
+// cuando hay un unico parametro se puede ahorrar el parentesis
+let cuadrado2 = y => {
+    return y * y;
+}
+console.log(`${cuadrado2(5)}`);
+
+//Otra forma de abreviarlo es quitando los parentesis y sin agregar un return, si solo devuelve un valor
+let cuadrado3 = z => z * z;
+console.log(`${cuadrado3(3)}`);
+
+//Refactor de los 3 saludos del principio en con los parametros de ES6 que aprendí
+let greet2 = () => console.log(`Saludos guardian`);
+greet2();
+let logGreeting2 = fn => fn();
+logGreeting2(greet2);
+let greetMe2 = () => console.log(`Los arboles te saludan desde greetMe2`);
+greetMe2();
+
