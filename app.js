87076112cd372fddeef7d82827ebde23777d9ce7
gamalielgon <73608213+gamalielgon@users.noreply.github.com>
2022-03-21 22:43:22 -0600

    Practica 9
    Practica 9 MIDDLEWARE EXPRESS

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
-//var Emitter = require('./emitter');
-var config = require('./config');
-var Emitter = require('events');
-//en la consola se ve que todo funciona bien y correcto
-// cambiaron algunas cosas como los nombres, y se agregaron otros eventos
-var emtr = new Emitter();
+const express = require('express');
+const app = express();
 
-emtr.on(config.events.GREET, () => {
-    console.log('Somewhere, someone said hello.');
+app.get('/', (req, res)=> {// route handler
+    res.send('Hello world, this is the root route');
 });
 
-emtr.on(config.events.GREET, () => {
-    console.log('A greeting occurred!');
+app.listen(3000);
+app.get('/uno', (req, res)=> { //route handler
+    res.send('Hello world, frome route One');
 });
-
-console.log('Hello!');
-emtr.emit(config.events.GREET);
-// lo que hicimos fue instanciar el "emisor de eventos"
-// a este emisor le agregamos 2 funciones
-// luego con el log mandamos un hola y luego llamamos a las funciones greet
-// las cuales devuelven el saludo
-
-emtr.on(config.events.JUMP, () => {
-    console.log('someone jumped!');
-});
-
-console.log(emtr);
-emtr.emit(config.events.JUMP);
-
-/* lo que arroja este codigo es la forma que tiene emtr
-emtr tiene dos eventos llamados greet ambas dentro del mismo array 
-y otro evento llamado jump, que esta en un array diferente.
-Ya que he hecho los cambios con el config, cambie todas las
-strings a llamadas al archivo config, para evitar el error.
-Lo que sucede en la terminal, es que todo funciona perfectamente
-ya que lo que antes era un string, ahora es una llamada a una string
-pero, si quisiera cambiarlo, solo tengo que cambiar el texto en 
-el config */
+//Esta funcion se manda a la ruta prueba y regresa codigo en html 
+app.get('/prueba', (req, res)=> { //route handler
+    res.send('<h1>Hola desde la prueba</h1><p>Esto es el cuerpo de la prueba</p>');
+})
